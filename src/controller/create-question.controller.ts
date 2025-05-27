7b98cc84b8d9972283115db559fab7d92c729ee6
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-05-27 12:59:57 -0300

    feat: create decorator to get sub from request

--- src/controller/create-question.controller.ts
+++ src/controller/create-question.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Post, UseGuards } from '@nestjs/common'
 
+import { CurrentUser } from '@/auth/current-user.decorator'
+import type { UserPayload } from '@/auth/jwt.strategy'
 import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
 
 @Controller('/questions')
@@ -7,7 +9,7 @@ import { JwtAuthGuard } from '@/auth/jwt-auth.guard'
 export class CreateQuestionController {
   @Post()
   // @UsePipes(new ZodValidationPipe())
-  async handler() {
-    return 'ok'
+  async handler(@CurrentUser() user: UserPayload) {
+    return user
   }
 }
