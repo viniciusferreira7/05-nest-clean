9145081eab3b4ecab8444c8a07ca2ad6b8fa50c9
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-06-17 09:31:12 -0300

    chore: create setup-e2e file to create isolated database in tests e2e

---
+++ test/setup-e2e.ts
@@ -0,0 +1,35 @@
+import { execSync } from 'child_process';
+import { randomUUID } from 'node:crypto';
+import "dotenv/config"
+import { PrismaClient } from 'generated/prisma'
+
+const prisma = new PrismaClient()
+
+
+function generateUniqueDatabaseURL(schemaId: string){
+  if(!process.env.DATABASE_URL){
+    throw new Error("Please provide a DATABASE_URL environment variable");
+    
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schemaId)
+
+  return url.toString()
+}
+
+const schemaId = randomUUID()
+
+beforeAll(async () => {
+
+  const databaseUrl = generateUniqueDatabaseURL(schemaId)
+
+  process.env.DATABASE_URL = databaseUrl
+
+  execSync("pnpm prisma migrate deploy")
+})
+
+beforeAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId} CASCADE"`)
+})
