e62a4ef71bbef4dd7f74b731c410be7e8f71eb8e
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-06-19 08:07:09 -0300

    build: add dockerfile

--- src/controller/create-account.controller.e2e-spec.ts
+++ src/controller/create-account.controller.e2e-spec.ts
@@ -1,3 +1,20 @@
-test('1 plus 1', () => {
-  expect(1 + 1).toEqual(2)
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+
+import { AppModule } from '@/app.module'
+
+describe('Create account (E2E)', () => {
+  let app: INestApplication
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+
+    await app.init()
+  })
+
+  test('[POST] -> /accounts', async () => {})
 })
--- test/setup-e2e.ts
+++ test/setup-e2e.ts
@@ -1,15 +1,15 @@
-import { execSync } from 'child_process';
-import { randomUUID } from 'node:crypto';
-import "dotenv/config"
+import 'dotenv/config'
+
+import { randomUUID } from 'node:crypto'
+
+import { execSync } from 'child_process'
 import { PrismaClient } from 'generated/prisma'
 
 const prisma = new PrismaClient()
 
-
-function generateUniqueDatabaseURL(schemaId: string){
-  if(!process.env.DATABASE_URL){
-    throw new Error("Please provide a DATABASE_URL environment variable");
-    
+function generateUniqueDatabaseURL(schemaId: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL environment variable')
   }
 
   const url = new URL(process.env.DATABASE_URL)
@@ -22,14 +22,14 @@ function generateUniqueDatabaseURL(schemaId: string){
 const schemaId = randomUUID()
 
 beforeAll(async () => {
-
   const databaseUrl = generateUniqueDatabaseURL(schemaId)
 
   process.env.DATABASE_URL = databaseUrl
 
-  execSync("pnpm prisma migrate deploy")
+  execSync('pnpm prisma migrate deploy')
 })
 
 beforeAll(async () => {
   await prisma.$executeRawUnsafe(`DROP SCHEMA IF EXISTS "${schemaId} CASCADE"`)
+  await prisma.$disconnect()
 })
