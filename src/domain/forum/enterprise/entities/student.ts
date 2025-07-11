8698bda368dc88ec2a2b10310b808960134c81cf
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-07-11 08:52:36 -0300

    feat: add use case to authenticate student

--- src/domain/forum/enterprise/entities/student.ts
+++ src/domain/forum/enterprise/entities/student.ts
@@ -8,6 +8,18 @@ interface StudentProps {
 }
 
 export class Student extends Entity<StudentProps> {
+  get name() {
+    return this.props.name
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  get password() {
+    return this.props.password
+  }
+
   static create(props: StudentProps, id?: UniqueEntityId) {
     const student = new Student(props, id)
 
