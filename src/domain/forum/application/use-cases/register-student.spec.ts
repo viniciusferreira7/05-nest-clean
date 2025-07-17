579ccd39b4f43551937748fb7fbe82fbe1f8efde
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-07-17 07:13:39 -0300

    feat: add in memoery students repository

---
+++ src/domain/forum/application/use-cases/register-student.spec.ts
@@ -0,0 +1,50 @@
+import { FakeHasher } from 'test/cryptography/fake-hasher'
+import { InMemoryStudentsRepository } from 'test/repositories/in-memory-students-repository'
+
+import { RegisterStudentUseCase } from './register-student'
+
+let inMemoryStudentsRepository: InMemoryStudentsRepository
+let fakerHasher: FakeHasher
+
+let sut: RegisterStudentUseCase
+
+describe('Register student', () => {
+  beforeEach(() => {
+    inMemoryStudentsRepository = new InMemoryStudentsRepository()
+    fakerHasher = new FakeHasher()
+
+    sut = new RegisterStudentUseCase(inMemoryStudentsRepository, fakerHasher)
+  })
+
+  it('should be able to register new student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toEqual(
+      expect.objectContaining({
+        student: {
+          name: 'John Doe',
+          email: 'john.doe@example.com',
+          password: '123456-hashed',
+        },
+      }),
+    )
+  })
+
+  it('should be able to register new student', async () => {
+    const result = await sut.execute({
+      name: 'John Doe',
+      email: 'john.doe@example.com',
+      password: '123456',
+    })
+
+    expect(result.isRight()).toEqual(
+      expect.objectContaining({
+        student: inMemoryStudentsRepository.items[0],
+      }),
+    )
+  })
+})
---
+++ test/repositories/in-memory-students-repository.ts
@@ -0,0 +1,16 @@
+import type { StudentsRepository } from '@/domain/forum/application/repositories/students-repository'
+import type { Student } from '@/domain/forum/enterprise/entities/student'
+
+export class InMemoryStudentsRepository implements StudentsRepository {
+  public items: Student[] = []
+
+  async findByEmail(email: string): Promise<Student | null> {
+    const student = this.items.find((item) => item.email.toString() === email)
+
+    return student ?? null
+  }
+
+  async create(student: Student): Promise<void> {
+    this.items.push(student)
+  }
+}
