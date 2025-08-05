e72a35697d20c8447eb855ba70c00010c5527863
Vinicius Ferreira <viniciuferreira32@gmail.com>
2025-08-05 09:38:28 -0300

    feat: add prisma question comment mapper

---
+++ src/infra/database/prisma/mappers/prisma-question-comment-mapper.ts
@@ -0,0 +1,36 @@
+import type { Comment as PrismaComment, Prisma } from 'generated/prisma'
+
+import { UniqueEntityId } from '@/core/entities/value-object/unique-entity-id'
+import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+
+export class PrismaQuestionCommentMapper {
+  static toDomain(raw: PrismaComment): QuestionComment {
+    if (!raw.questionId) {
+      throw new Error('Invalid comment type.')
+    }
+
+    return QuestionComment.create(
+      {
+        content: raw.content,
+        authorId: new UniqueEntityId(raw.authorId),
+        questionId: new UniqueEntityId(raw.questionId),
+        createdAt: raw.createdAt,
+        updatedAt: raw.updatedAt,
+      },
+      new UniqueEntityId(raw.id),
+    )
+  }
+
+  static toPrisma(
+    questionComment: QuestionComment,
+  ): Prisma.CommentUncheckedCreateInput {
+    return {
+      id: questionComment.id.toString(),
+      authorId: questionComment.authorId.toString(),
+      content: questionComment.content,
+      questionId: questionComment?.questionId.toString(),
+      createdAt: questionComment.createdAt,
+      updatedAt: questionComment.updatedAt,
+    }
+  }
+}
