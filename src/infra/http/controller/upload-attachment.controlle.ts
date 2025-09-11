af7c37cc1905049c11d4b68f95a594de8d27613f
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-09-11 06:58:09 -0300

    chore: apply improvements on build and deploy step

---
+++ src/infra/http/controller/upload-attachment.controlle.ts
@@ -0,0 +1,10 @@
+import { Controller, Post } from "@nestjs/common";
+
+@Controller("/attachments")
+export class UploadAttachmentBySlugController {
+	// constructor(
+	// ) {}
+
+	@Post()
+	async handler() {}
+}
