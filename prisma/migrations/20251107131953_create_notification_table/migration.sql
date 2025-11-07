256d3c3b396af48e6b9fa75d89c763017ca90c00
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-11-07 10:21:12 -0300

    feat: create notifications table

---
+++ prisma/migrations/20251107131953_create_notification_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "notifications" (
+    "id" TEXT NOT NULL,
+    "recipientId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "read_at" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "notifications_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "notifications" ADD CONSTRAINT "notifications_recipientId_fkey" FOREIGN KEY ("recipientId") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
