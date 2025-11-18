3a82ea7128487d84343525e5b9f83851116b30d1
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-11-18 10:29:27 -0300

    test: create test to event on answer created

--- src/infra/events/events.module.ts
+++ src/infra/events/events.module.ts
@@ -1,5 +1,6 @@
 import { Module } from "@nestjs/common";
 import { SendNotificationUseCase } from "@/domain/notification/application/use-cases/send-notification";
+import { OnAnswerCommentCreated } from "@/domain/notification/subscribers/on-answer-comment-created";
 import { OnAnswerCreated } from "@/domain/notification/subscribers/on-answer-created";
 import { OnQuestionBestAnswerChosenCreated } from "@/domain/notification/subscribers/on-question-best-answer-chosen";
 import { OnQuestionCommentCreated } from "@/domain/notification/subscribers/on-question-comment-created";
@@ -11,6 +12,7 @@ import { DatabaseModule } from "../database/database.module";
 		OnAnswerCreated,
 		OnQuestionBestAnswerChosenCreated,
 		OnQuestionCommentCreated,
+		OnAnswerCommentCreated,
 		SendNotificationUseCase,
 	],
 })
--- src/infra/events/on-answer-created.e2e-spec.ts
+++ src/infra/events/on-answer-created.e2e-spec.ts
@@ -3,17 +3,15 @@ import { JwtService } from "@nestjs/jwt";
 import request from "supertest";
 import { makeModuleRef } from "test/factories/make-module-ref";
 import { QuestionFactory } from "test/factories/make-question";
-import { QuestionCommentFactory } from "test/factories/make-question-comment";
 import { StudentFactory } from "test/factories/make-student";
-import { test } from "vitest";
+import { waitFor } from "test/utils/wait-for";
 import { PrismaService } from "@/infra/database/prisma/prisma.service";
 
-describe("Comment on question (E2E)", () => {
+describe("On answer created (E2E)", () => {
 	let app: INestApplication;
 	let prisma: PrismaService;
 	let studentFactory: StudentFactory;
 	let questionFactory: QuestionFactory;
-	let questionCommentFactory: QuestionCommentFactory;
 	let jwt: JwtService;
 
 	beforeAll(async () => {
@@ -23,13 +21,11 @@ describe("Comment on question (E2E)", () => {
 		prisma = moduleRef.get(PrismaService);
 		studentFactory = moduleRef.get(StudentFactory);
 		questionFactory = moduleRef.get(QuestionFactory);
-		questionCommentFactory = moduleRef.get(QuestionCommentFactory);
 		jwt = moduleRef.get(JwtService);
-
 		await app.init();
 	});
 
-	test("[POST]: /questions/:questionId/comments", async () => {
+	it("should be send a notification when answer is created", async () => {
 		const user = await studentFactory.makePrismaStudent();
 		const question = await questionFactory.makePrismaQuestion({
 			authorId: user.id,
@@ -37,30 +33,25 @@ describe("Comment on question (E2E)", () => {
 
 		const accessToken = jwt.sign({ sub: user.id.toString() });
 
+		//TODO: add place to put OnAnswerCreate event
+
 		const response = await request(app.getHttpServer())
-			.post(`/questions/${question.id}/comments`)
+			.post(`/questions/${question.id}/answers`)
 			.send({
-				content: "Comment on question content",
+				content: "Answer question content",
+				attachments: [],
 			})
 			.set("Authorization", `Bearer ${accessToken}`);
 
-		expect(response.statusCode).toBe(201);
+		await waitFor(async () => {
+			const notificationOnDatabase = await prisma.notification.findMany({
+				where: {
+					recipientId: user.id.toString(),
+				},
+			});
 
-		const questionCommentOnDatabase = await prisma.comment.findFirst({
-			where: {
-				authorId: user.id.toString(),
-				questionId: question.id.toString(),
-				content: "Comment on question content",
-			},
+			expect(notificationOnDatabase).not.toBeNull();
 		});
-
-		expect(questionCommentOnDatabase).toEqual(
-			expect.objectContaining({
-				content: "Comment on question content",
-				authorId: user.id.toString(),
-				questionId: question.id.toString(),
-			}),
-		);
 	});
 
 	afterAll(async () => {
