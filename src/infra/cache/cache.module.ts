7e5fc175523dec70b22c2de22ff7bd1262c57b3a
Vinicius De Aquino Ferreira <viniciusaquinodev07@gmail.com>
2025-12-02 10:01:31 -0300

    feat: implement redis cache repository

--- src/infra/cache/cache.module.ts
+++ src/infra/cache/cache.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
+import { CacheRepository } from "./cache-repository";
 import { RedisService } from "./redis/redis.service";
+import { RedisCacheRepository } from "./redis/redis-cache-repository";
 
 @Module({
-	providers: [RedisService],
-	exports: [RedisService],
+	providers: [
+		RedisService,
+		{
+			provide: CacheRepository,
+			useClass: RedisCacheRepository,
+		},
+	],
+	exports: [RedisService, CacheRepository],
 })
 export class CacheModule {}
